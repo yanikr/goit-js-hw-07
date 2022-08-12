99e10a53b805c6e918ec94698f6d5fb5b387eedd
Yanik Rikhter <rihter.yan@gmail.com>
2022-08-12 15:34:25 +0300

    tsk 1 done
    +++

---
+++ js/01-gallery.js
@@ -0,0 +1,38 @@
+import { galleryItems } from "./gallery-items.js";
+
+// Change code below this line
+
+console.log(galleryItems);
+const galSel = document.querySelector(".gallery");
+
+galSel.classList.add("gallery__item");
+
+const imagesMap = galleryItems
+  .map(
+    (technology) =>
+      `<a class="gallery__link" href="${technology.original}"><img class="gallery__image" src=${technology.preview} data-source=${technology.original} alt=${technology.description}></img></a>`
+  )
+  .join("");
+
+galSel.insertAdjacentHTML("beforeend", imagesMap);
+
+galSel.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const instanceCreate = basicLightbox.create(
+    `<img src="${evt.target.closest("img").dataset.source}">`
+  );
+
+  instanceCreate.show();
+  document.addEventListener("keydown", (evt) => {
+    const visible = basicLightbox.visible();
+    if (visible === false) {
+      return;
+    }
+    if (evt.code === "Escape") {
+      instanceCreate.close();
+    }
+  });
+});
